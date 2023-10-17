30f4e27f07f6a58c7a1aa0a5b10b3298010c6aac
Roman Batsenko <rbatsenko@gmail.com>
2023-10-17 18:17:59 +0200

    chore: remove redirect

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/*",
-        destination: "/coach",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
