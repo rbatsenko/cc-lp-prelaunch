237ce99865bd1a169b4d559586802f2d699976c8
Roman Batsenko <rbatsenko@gmail.com>
2023-10-18 23:37:18 +0200

    chore: fix form styles

--- src/app/coaches/_components/Form.tsx
+++ src/app/coaches/_components/Form.tsx
@@ -32,7 +32,7 @@ const Gradient = () => (
 export const Form = () => (
   <div className="bg-white py-16 sm:pt-24 sm:pb-16" id="signup">
     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div className="relative isolate flex flex-col gap-10 overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:flex-row xl:items-center xl:py-32">
+      <div className="relative isolate flex flex-col items-center gap-10 overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:flex-row xl:py-32">
         <h2 className="max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl xl:max-w-none xl:flex-auto">
           Join as a Coach today.
         </h2>
