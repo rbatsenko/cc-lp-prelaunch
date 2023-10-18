7a0cbcba7585174fb1e0d6382db62dd2f04f9738
Roman Batsenko <rbatsenko@gmail.com>
2023-10-18 13:00:12 +0200

    chore: add redirect from home page

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/coaches",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
